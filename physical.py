6e3cf1daa109b18327c71a0195dd87f30eb6b34f
Andrew Fowlie <andrew.j.fowlie@qq.com>
2018-09-17 17:39:03 +1000

    add scan

--- physical.py
+++ physical.py
@@ -9,29 +9,57 @@ export ANYBUBBLE=/PATH/TO/AnyBubble
 export PYTHONPATH=/PATH/TO/CosmoTransitions:/PATH/TO/BubbleProfiler/examples/sm-plus-singlet
 """
 
+import numpy as np
+
 from bubbler import bubblers, Potential
 from sm_plus_singlet import generate_potential
 
-# Works
 
-T = 85.
-Tc = 110.
-lambda_m = 1.5
-lambda_s = 0.688259
+BACKENDS = ["cosmotransitions", "bubbleprofiler", "anybubble"]
 
-# Fails
 
-# lambda_s = 1.76759
+def physical(T, T_C, lambda_m, lambda_s, backends=None):
 
-p_dict = generate_potential(T, Tc, lambda_m, lambda_s)
-tv = p_dict["true_vac"][::-1]  # Reverse them as my code likes the fields in alphabetical order!
-fv = p_dict["false_vac"][::-1]
-p = p_dict["potential_str"]
+    p_dict = generate_potential(T, T_C, lambda_m, lambda_s)
+    tv = p_dict["true_vac"][::-1]  # Reverse them as my code likes the fields in alphabetical order!
+    fv = p_dict["false_vac"][::-1]
+    p = p_dict["potential_str"]
 
-potential = Potential(p, true_vacuum=tv, false_vacuum=fv)
+    potential = Potential(p, true_vacuum=tv, false_vacuum=fv)
 
-results = bubblers(potential, backends=["cosmotransitions", "bubbleprofiler", "anybubble"])
+    backends = BACKENDS if not backends else backends
+    results = bubblers(potential, backends=backends)
 
-for result in results.itervalues():
-    print "backend = {}. action = {}. time = {}".format(result.backend, result.action, result.time)
-    print "command = {}".format(result.command)
+    return results
+
+
+if __name__ == "__main__":
+
+    # Works
+    print physical(85., 110., 1.5, 0.688259)
+
+    # Fails
+    print physical(85., 110., 1.5,  1.76759)
+
+    # Scan in final coupling
+
+    lambda_ = np.linspace(0., 2., 100)
+
+    n_total = n_b = n_c = 0
+
+    for l in lambda_:
+
+        try:
+            r = physical(85., 110., 1.5, l, backends=["cosmotransitions", "bubbleprofiler"])
+        except:
+            continue
+
+        n_total += 1
+
+        if r["bubbleprofiler"].action:
+            n_b += 1
+        if r["cosmotransitions"].action:
+            n_c += 1
+
+    print "cosmo", float(n_c) / n_total
+    print "us", float(n_c) / n_total
